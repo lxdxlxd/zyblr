704a4e6a7bdc5bd7a61bca69560feccb91566911
Ran-Liu-lr <laochlan@gmail.com>
2017-03-28 17:19:45 -0400

    Update player.cc

--- player.cc
+++ player.cc
@@ -22,3 +22,17 @@ void player::setname(string inputname){
         name = inputname;
 }
 
+void player::changehealth(string how, int much){
+    if (how == "plus"){
+        health += much;
+    } else {
+        health -= much;
+    }
+}
+void player::changemagic(string how, int much){
+    if (how == "plus"){
+        magic += much;
+    } else {
+        magic -= much;
+    }
+}
